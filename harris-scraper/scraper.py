ec8502a1a75004d2027b9902e9ef003f954116f5
Simone Brancato <simonebrancato18@gmail.com>
2024-09-30 01:05:43 +0200

    Scraping both timestamp and text (but retrieving very old posts after a while, skipping more recent posts)

--- harris-scraper/scraper.py
+++ harris-scraper/scraper.py
@@ -7,18 +7,26 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import mysql.connector
+from datetime import datetime
 import time
 import os
 
 def setup_driver():
     chrome_options = Options()
+    
     chrome_options.add_argument("--headless=new")  
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("window-size=1920x1080")
     chrome_options.add_argument('--disable-gpu')  
     chrome_options.add_argument('--disable-extensions') 
-    chrome_options.add_argument('--incognito')  
+    chrome_options.add_argument('--incognito')
+    chrome_options.add_experimental_option("prefs", {"profile.default_content_setting_values.notifications" : 2})
+    chrome_options.add_argument("--disable-search-engine-choice-screen")
+    chrome_options.add_argument("--lang=en")
+    chrome_options.add_argument(f'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36')
+    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     return driver
 
@@ -55,10 +63,12 @@ def handle_login():
     
 handle_login()
 
+# Pause to load homepage, then go to Harris profile and wait again
 time.sleep(20)
-driver.get("http://www.facebook.com/KamalaHarris")  
+driver.get("http://www.facebook.com/KamalaHarris")
 time.sleep(20)
 
+# Establish connection with Elections database
 connection = mysql.connector.connect(
     host='mysql',
     port='3306', 
@@ -73,42 +83,74 @@ print("Connected to the database!")
 cursor = connection.cursor()
 
 def scroll_down():
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    driver.execute_script("window.scrollBy(0, 800);")
     time.sleep(20)
 
-def scrape_posts():
+# Parses FB timestamp format to datetime format
+def parse_timestamp_to_datetime(timestamp):
+    current_timestamp_format = "%A, %B %d, %Y at %I:%M %p"
 
-    post_texts = set()  # Set to track scraped posts 
+    try:
+        dt_object = datetime.strptime(timestamp, current_timestamp_format)
+        mysql_datetime = dt_object.strftime("%Y-%m-%d %H:%M:%S")
+    except Exception:
+        mysql_datetime = "2020-01-01 12:00:00"
+
+    return mysql_datetime
+
+
+action = webdriver.ActionChains(driver) # To extract timestamp from tooltip
+
+# Scrapes the first 100 posts, extracting timestamp and text
+def scrape_posts():
+    posts_txt = set()  # Set to track scraped posts 
     
-    while len(post_texts) < 100:
-        scroll_down()
-        time.sleep(20)  # Add a pause to load the page (Low performance, 20s to be sure)
+    while len(posts_txt) < 100:
         
+        time.sleep(30)  # Add a long pause to load the page due to low performances
+
         # Search for new posts
         new_posts = WebDriverWait(driver, 30).until(
-            EC.presence_of_all_elements_located((By.XPATH, "//div[@class='x1l90r2v x1pi30zi x1swvt13 x1iorvi4']"))
+            EC.presence_of_all_elements_located((By.XPATH, "//div[@class='x1yztbdb x1n2onr6 xh8yej3 x1ja2u2z']"))
         )
 
-        new_texts = []  # To memorize new text in each round
+        print(f"Retrieved {len(new_posts)} posts.")
 
         for new_post in new_posts:
-            text = new_post.text.strip()
-            
-            # Add text only if it is at least 5 characters long and if it has not already been seen
-            if len(text)>=5 and text not in post_texts:    
-                    new_texts.append((text,))
-                    post_texts.add(text) 
+            timestamp_str = "NULL"
+            text = "NULL"
 
-        # If there are new texts add them to the DB
-        if new_texts:
-            sql_insert_query = """INSERT IGNORE INTO Harris (content) VALUES (%s)"""
-            cursor.executemany(sql_insert_query, new_texts)
-            connection.commit()
-            print(f"Inserted {len(new_texts)} values into Harris table.")
-        else:
-            print("No new posts found.")
-    
-    return post_texts
+            try:                                        
+                text = new_post.find_element(By.XPATH, ".//span[@class='x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x3x7a5m x6prxxf xvq8zen xo1l8bm xzsf02u x1yc453h']").get_attribute('innerText')
+            except Exception:
+                print("#### SKIPPING CURRENT POST ####")
+
+            if text != "NULL" and text not in posts_txt:
+                try:
+                    extracted_timestamp = new_post.find_element(By.XPATH, ".//span[@class='x1rg5ohu x6ikm8r x10wlt62 x16dsc37 xt0b8zv']")
+                    action.move_to_element(extracted_timestamp).perform()
+                    time.sleep(5)
+
+                    timestamp_str = WebDriverWait(new_post, 20).until(
+                        EC.presence_of_element_located((By.XPATH, "//div[@class='xj5tmjb x1r9drvm x16aqbuh x9rzwcf xjkqk3g xms15q0 x1lliihq xo8ld3r xjpr12u xr9ek0c x86nfjv xz9dl7a xsag5q8 x1ye3gou xn6708d x1n2onr6 x19991ni __fb-dark-mode  x1hc1fzr xhb22t3 xls3em1']"))
+                    ).get_attribute('innerText')
+
+                except Exception:
+                    pass
+
+                if timestamp_str != "NULL":
+                    timestamp = parse_timestamp_to_datetime(timestamp_str)
+                    posts_txt.add(text)
+
+                    sql_insert_query = """INSERT IGNORE INTO Harris (timestamp,content) VALUES (%s, %s)"""
+                    cursor.execute(sql_insert_query, (timestamp,text))
+                    connection.commit()
+
+                    print("Inserted row into Harris table.")
+
+        scroll_down()
+
+    return posts_txt
 
 posts = scrape_posts()
 
--- mysql/init-db.sql
+++ mysql/init-db.sql
@@ -3,12 +3,14 @@ USE Elections;
 
 CREATE TABLE IF NOT EXISTS Trump (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    timestamp DATETIME,
     content VARCHAR(1000) NOT NULL,
     UNIQUE (content(100))
 );
 
 CREATE TABLE IF NOT EXISTS Harris (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    timestamp DATETIME,
     content VARCHAR(1000) NOT NULL,
     UNIQUE (content(100))
 );
