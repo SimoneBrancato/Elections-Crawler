43f680d85111a117ca77dffd2970da874ef9684c
Simone Brancato <simonebrancato18@gmail.com>
2024-10-09 12:56:32 +0200

    Refactoring

--- harris-scraper/scraper.py
+++ harris-scraper/scraper.py
@@ -38,6 +38,7 @@ def setup_driver():
 driver = setup_driver()
 driver.maximize_window()
 driver.get("http://www.facebook.com")
+action = webdriver.ActionChains(driver)
 
 # Accepts all cookies pop-up
 def handle_cookie():
@@ -48,8 +49,9 @@ def handle_cookie():
 handle_cookie()
 
 load_dotenv('.env')
-FB_EMAIL_HARRIS: str = os.getenv('FB_EMAIL_HARRIS')
-FB_PASSWORD_HARRIS: str = os.getenv('FB_PASSWORD_HARRIS')
+FB_EMAIL: str = os.getenv('FB_EMAIL_HARRIS')
+FB_PASSWORD: str = os.getenv('FB_PASSWORD_HARRIS')
+CANDIDATE: str = "KamalaHarris"
 
 # Targets email and password forms, fills them with email and password, targets submit button and clicks it
 def handle_login():
@@ -57,9 +59,9 @@ def handle_login():
     password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='pass']")))
     
     email.clear()
-    email.send_keys(FB_EMAIL_HARRIS)
+    email.send_keys(FB_EMAIL)
     password.clear()
-    password.send_keys(FB_PASSWORD_HARRIS)
+    password.send_keys(FB_PASSWORD)
     time.sleep(1)
 
     submit_button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']")))
@@ -68,9 +70,9 @@ def handle_login():
     
 handle_login()
 
-# Pause to load homepage, then go to Harris profile and wait again
+# Pause to load homepage, then go to profile and wait again
 time.sleep(20)
-driver.get("http://www.facebook.com/KamalaHarris")
+driver.get(f"http://www.facebook.com/{CANDIDATE}")
 time.sleep(20)
 
 # Establish connection with Elections database
@@ -82,32 +84,27 @@ connection = mysql.connector.connect(
     database='Elections'  
 )
 
+cursor = connection.cursor() # To execute SQL queries
+
 print("Connected to the database!")
 
-# Cursor to execute SQL queries
-cursor = connection.cursor()
-
+# Scroll down into page
 def scroll_down():
     driver.execute_script("window.scrollBy(0, 800);")
     time.sleep(25)
 
+# Scroll down into popup element
 def scroll_popup(element):
     driver.execute_script("arguments[0].scrollBy(0, 800);", element)
     time.sleep(10)
 
-action = webdriver.ActionChains(driver) # To extract timestamp from tooltip
-
-# Download image from an image_url
-def download_image(image_url):
-    response = requests.get(image_url)
-    img = Image.open(BytesIO(response.content))
-    return img
-
-# Step 2: Apply OCR
+# Extracts text from image
 def extract_image_text_from_post(post):
     try:
-        image_url = post.find_element(By.XPATH, ".//div[@class='x10l6tqk x13vifvy']//img").get_attribute('src')
-        img = download_image(image_url)
+        xpath_image = ".//div[@class='x10l6tqk x13vifvy']//img"
+        image_url = post.find_element(By.XPATH, xpath_image).get_attribute('src')
+        response = requests.get(image_url)
+        img = Image.open(BytesIO(response.content))
         text = pytesseract.image_to_string(img)
         return " " + text
     except Exception:
@@ -116,7 +113,8 @@ def extract_image_text_from_post(post):
 # Extracts caption from text
 def extract_caption_from_post(post):
     try:
-        caption = post.find_element(By.XPATH, ".//span[@class='x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x3x7a5m x6prxxf xvq8zen xo1l8bm xzsf02u x1yc453h']").get_attribute('innerText')
+        xpath_caption_element = ".//span[@class='x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x3x7a5m x6prxxf xvq8zen xo1l8bm xzsf02u x1yc453h']"
+        caption = post.find_element(By.XPATH, xpath_caption_element).get_attribute('innerText')
         return caption
     except Exception:
         return ""
@@ -126,40 +124,133 @@ def get_text_from_post(post):
     try:
         caption = extract_caption_from_post(post)                                        
         image_text = extract_image_text_from_post(post)
-        text = caption + image_text                                        
-
-        query = """
-                SELECT uuid 
-                FROM Posts 
-                WHERE content LIKE %s
-                AND candidate = 'Harris'
-                LIMIT 1
-                """
-        cursor.execute(query, (text,))
-        result = cursor.fetchone()
-        uuid = result[0] if result else 0 
+        text = caption + image_text
+        return text
     except Exception:
-        return None, None
+        return None
+
+# Returns post uuid if text is in db, else returns 0
+def search_post_into_database(text):
+    query = """
+            SELECT uuid 
+            FROM Posts 
+            WHERE content LIKE %s
+            AND candidate = %s
+            LIMIT 1
+            """
+    cursor.execute(query, (text, CANDIDATE))
+    result = cursor.fetchone()
+    uuid = result[0] if result else 0 
     
-    return text, uuid
+    return uuid
 
 # Ectract timestamp from post
 def get_timestamp_from_post(post):
     try:
-        extracted_timestamp = post.find_element(By.XPATH, ".//span[@class='x1rg5ohu x6ikm8r x10wlt62 x16dsc37 xt0b8zv']")
+        xpath_timestamp = ".//span[@class='x1rg5ohu x6ikm8r x10wlt62 x16dsc37 xt0b8zv']"
+        extracted_timestamp = post.find_element(By.XPATH, xpath_timestamp)
         action.move_to_element(extracted_timestamp).perform()
         time.sleep(2)
 
+        xpath_timestamp_tooltip = "//div[@class='xj5tmjb x1r9drvm x16aqbuh x9rzwcf xjkqk3g xms15q0 x1lliihq xo8ld3r xjpr12u xr9ek0c x86nfjv xz9dl7a xsag5q8 x1ye3gou xn6708d x1n2onr6 x19991ni __fb-dark-mode  x1hc1fzr xhb22t3 xls3em1']"
         timestamp_str = WebDriverWait(post, 20).until(
-            EC.presence_of_element_located((By.XPATH, "//div[@class='xj5tmjb x1r9drvm x16aqbuh x9rzwcf xjkqk3g xms15q0 x1lliihq xo8ld3r xjpr12u xr9ek0c x86nfjv xz9dl7a xsag5q8 x1ye3gou xn6708d x1n2onr6 x19991ni __fb-dark-mode  x1hc1fzr xhb22t3 xls3em1']"))
+            EC.presence_of_element_located((By.XPATH, xpath_timestamp_tooltip))
         ).get_attribute('innerText')
         
         timestamp = datetime.strptime(timestamp_str, "%A, %B %d, %Y at %I:%M %p")
+        return timestamp
     except Exception:
         return None
 
-    return timestamp
+# Extract comments from post
+def get_comments_from_post(post, post_uuid, max_comments):
+    try:
+        xpath_comment_button = ".//div[@class='x1i10hfl x1qjc9v5 xjqpnuy xa49m3k xqeqjp1 x2hbi6w x1ypdohk xdl72j9 x2lah0s xe8uvvx x2lwn1j xeuugli x1hl2dhg xggy1nq x1t137rt x1o1ewxj x3x9cwd x1e5q0jg x13rtm0m x3nfvp2 x1q0g3np x87ps6o x1lku1pv x1a2a7pz xjyslct xjbqb8w x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1heor9g xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 x16tdsg8 x1ja2u2z']"
+        comments_button = post.find_element(By.XPATH, xpath_comment_button)
+        comments_button.click()
+        time.sleep(5)
 
+        xpath_post_popup = ".//div[@class='xb57i2i x1q594ok x5lxg6s x78zum5 xdt5ytf x6ikm8r x1ja2u2z x1pq812k x1rohswg xfk6m8 x1yqm8si xjx87ck xx8ngbg xwo3gff x1n2onr6 x1oyok0e x1odjw0f x1iyjqo2 xy5w88m']"
+        post_popup = driver.find_element(By.XPATH, xpath_post_popup)
+        
+        comments_count = 0
+
+        while comments_count < max_comments:
+            scroll_popup(post_popup)
+            xpath_comments = ".//div[@class='x1lliihq xjkvuk6 x1iorvi4']"
+            new_comments = post_popup.find_elements(By.XPATH, xpath_comments)
+
+            for new_comment in new_comments:
+                comment_text = new_comment.text
+                query = """
+                        SELECT COUNT(*) 
+                        FROM Comments 
+                        WHERE content LIKE %s
+                        """
+                cursor.execute(query, (comment_text,))
+                count = cursor.fetchone()[0]
+
+                if count > 0:
+                    continue
+
+                comment_uuid = str(uuid.uuid4())
+                
+                sql_insert_comment = """
+                                    INSERT IGNORE
+                                    INTO Comments (uuid, post_id, content)
+                                    VALUES(%s, %s, %s)
+                                    """
+                        
+                cursor.execute(sql_insert_comment, (comment_uuid, post_uuid, comment_text))
+                connection.commit()
+                comments_count += 1
+                time.sleep(1)
+    except Exception:
+        return
+            
+    print(f"Retrieved {comments_count} comments")
+    xpath_close_comments_button = "//div[@class='x1i10hfl xjqpnuy xa49m3k xqeqjp1 x2hbi6w x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x1ypdohk xdl72j9 x2lah0s xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r x2lwn1j xeuugli x16tdsg8 x1hl2dhg xggy1nq x1ja2u2z x1t137rt x1q0g3np x87ps6o x1lku1pv x1a2a7pz x6s0dn4 xzolkzo x12go9s9 x1rnf11y xprq8jg x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x78zum5 xl56j7k xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 xc9qbxq x14qfxbe x1qhmfi1']"
+    close_button = driver.find_element(By.XPATH, xpath_close_comments_button)
+    close_button.click()
+
+# Extracts reactions from post
+def update_post_reactions(post, text):
+    try:
+        xpath_reactions_button = ".//div[@class='x1i10hfl xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1o1ewxj x3x9cwd x1e5q0jg x13rtm0m x1n2onr6 x87ps6o x1lku1pv x1a2a7pz x1heor9g xnl1qt8 x6ikm8r x10wlt62 x1vjfegm x1lliihq']"
+        reactions_button = post.find_element(By.XPATH, xpath_reactions_button)
+        reactions_button.click()
+
+        time.sleep(3)
+        
+        xpath_reaction_panels = ".//div[@class='x1i10hfl xe8uvvx xggy1nq x1o1ewxj x3x9cwd x1e5q0jg x13rtm0m x87ps6o x1lku1pv x1a2a7pz xjyslct xjbqb8w x18o3ruo x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1heor9g x1ypdohk xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 x16tdsg8 x1hl2dhg x1vjfegm x3nfvp2 xrbpyxo x1itg65n x16dsc37']"
+        reaction_panels = driver.find_elements(By.XPATH, xpath_reaction_panels)
+        
+        for panel in reaction_panels:
+            reaction_str = panel.get_attribute("aria-label")
+            reaction_type, reaction_count = reaction_str.replace(" ","").lower().split(',')
+
+            if reaction_type == "all":
+                continue
+            
+            reaction_count_int = convert_reaction_count(reaction_count)
+
+            query_update_reactions = f"""
+                                    UPDATE Posts
+                                    SET `{reaction_type}` = %s 
+                                    WHERE candidate = %s 
+                                    AND content LIKE %s
+                                    """
+            cursor.execute(query_update_reactions, (reaction_count_int, CANDIDATE , f"%{text}%"))
+            connection.commit()
+
+        xpath_close_reactions_button = ".//div[@class='x1i10hfl xjqpnuy xa49m3k xqeqjp1 x2hbi6w x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x1ypdohk xdl72j9 x2lah0s xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r x2lwn1j xeuugli x16tdsg8 x1hl2dhg xggy1nq x1ja2u2z x1t137rt x1q0g3np x87ps6o x1lku1pv x1a2a7pz x6s0dn4 xzolkzo x12go9s9 x1rnf11y xprq8jg x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x78zum5 xl56j7k xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 xc9qbxq x14qfxbe x1qhmfi1']"
+        close_reactions_button = driver.find_element(By.XPATH, xpath_close_reactions_button)
+        close_reactions_button.click()
+        time.sleep(5)
+    except Exception:
+        return
+
+# Convert reaction count from string format to integer
 def convert_reaction_count(reaction_count):
     if 'k' in reaction_count: 
         return int(float(reaction_count.replace('k', '')) * 1000)
@@ -171,32 +262,30 @@ def convert_reaction_count(reaction_count):
 # Scrapes posts published in 2024 and sends to Elections DB 
 def scrape_posts():
     
-    inserted_timestamp = datetime.now()
+    retrieved_timestamp = datetime.now()
 
-    while inserted_timestamp > datetime(2024, 1, 1, 0, 0, 0):
-
+    while retrieved_timestamp > datetime(2024, 1, 1, 0, 0, 0):
+        
+        xpath_post_panels = "//div[@class='x1yztbdb x1n2onr6 xh8yej3 x1ja2u2z']"
         new_posts = WebDriverWait(driver, 50).until(
-            EC.visibility_of_all_elements_located((By.XPATH, "//div[@class='x1yztbdb x1n2onr6 xh8yej3 x1ja2u2z']"))
+            EC.visibility_of_all_elements_located((By.XPATH, xpath_post_panels))
         )
 
         print(f"Retrieved {len(new_posts)} posts.")
 
         for new_post in new_posts:
-
-            text, post_uuid = get_text_from_post(new_post)
-
+            text = get_text_from_post(new_post)
+            
             if text is None:
                 continue 
+
+            post_uuid = search_post_into_database(text)
 
             if post_uuid == 0:
                 timestamp = get_timestamp_from_post(new_post)
 
                 if timestamp is None:
                     continue 
-
-                if timestamp < datetime(2024, 1, 1, 0, 0, 0):
-                    print(f"### RETRIEVED VERY OLD POST {timestamp} ###")
-                    return
                 
                 post_uuid = str(uuid.uuid4()) # Generate UUID for current post
                 
@@ -205,79 +294,21 @@ def scrape_posts():
                                     INTO Posts (uuid, timestamp, candidate, content)
                                     VALUES (%s, %s, %s, %s)
                                     """
-                
-                cursor.execute(sql_insert_post, (post_uuid, timestamp, "Harris", text))
+                retrieved_timestamp = timestamp
+                cursor.execute(sql_insert_post, (post_uuid, timestamp, CANDIDATE, text))
                 connection.commit()
                                     
-                print(f"Inserted row into Harris table. Timestamp: {timestamp}.")
-                inserted_timestamp = timestamp     
+                print(f"Inserted row into table. Candidate: {CANDIDATE} Timestamp: {timestamp}.")
 
-            # UPDATE REACTIONS COUNT
-            reactions_button = new_post.find_element(By.XPATH, ".//div[@class='x1i10hfl xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1o1ewxj x3x9cwd x1e5q0jg x13rtm0m x1n2onr6 x87ps6o x1lku1pv x1a2a7pz x1heor9g xnl1qt8 x6ikm8r x10wlt62 x1vjfegm x1lliihq']")
-            reactions_button.click()
+                print("Loading reactions ...")
+                update_post_reactions(new_post, text)
+                print("Getting comments ...")
+                get_comments_from_post(new_post, post_uuid, max_comments=120)
 
-            time.sleep(5)
-            
-            reactions_tab = driver.find_elements(By.XPATH, ".//div[@class='x1i10hfl xe8uvvx xggy1nq x1o1ewxj x3x9cwd x1e5q0jg x13rtm0m x87ps6o x1lku1pv x1a2a7pz xjyslct xjbqb8w x18o3ruo x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1heor9g x1ypdohk xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 x16tdsg8 x1hl2dhg x1vjfegm x3nfvp2 xrbpyxo x1itg65n x16dsc37']")
-            
-            for tab in reactions_tab:
-                reaction_str = tab.get_attribute("aria-label")
-                reaction_type, reaction_count = reaction_str.replace(" ","").lower().split(',')
+            else:
+                print("Post present in DB. Updating reactions ...")    
+                update_post_reactions(new_post, text)
 
-                if reaction_type == "all":
-                    continue
-                
-                reaction_count_int = convert_reaction_count(reaction_count)
-
-                cursor.execute(f"UPDATE Posts SET `{reaction_type}` = %s WHERE candidate = 'Harris' AND content LIKE %s", (reaction_count_int, f"%{text}%"))
-                connection.commit()
-
-            close_reactions_button = driver.find_element(By.XPATH, ".//div[@class='x1i10hfl xjqpnuy xa49m3k xqeqjp1 x2hbi6w x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x1ypdohk xdl72j9 x2lah0s xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r x2lwn1j xeuugli x16tdsg8 x1hl2dhg xggy1nq x1ja2u2z x1t137rt x1q0g3np x87ps6o x1lku1pv x1a2a7pz x6s0dn4 xzolkzo x12go9s9 x1rnf11y xprq8jg x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x78zum5 xl56j7k xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 xc9qbxq x14qfxbe x1qhmfi1']")
-            close_reactions_button.click()
-            time.sleep(5)
-
-            # TAKE COMMENTS
-            comments_button = new_post.find_element(By.XPATH, ".//div[@class='x1i10hfl x1qjc9v5 xjqpnuy xa49m3k xqeqjp1 x2hbi6w x1ypdohk xdl72j9 x2lah0s xe8uvvx x2lwn1j xeuugli x1hl2dhg xggy1nq x1t137rt x1o1ewxj x3x9cwd x1e5q0jg x13rtm0m x3nfvp2 x1q0g3np x87ps6o x1lku1pv x1a2a7pz xjyslct xjbqb8w x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1heor9g xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 x16tdsg8 x1ja2u2z']")
-            comments_button.click()
-            time.sleep(5)
-
-            post_popup = driver.find_element(By.XPATH, ".//div[@class='xb57i2i x1q594ok x5lxg6s x78zum5 xdt5ytf x6ikm8r x1ja2u2z x1pq812k x1rohswg xfk6m8 x1yqm8si xjx87ck xx8ngbg xwo3gff x1n2onr6 x1oyok0e x1odjw0f x1iyjqo2 xy5w88m']")
-            
-            comments_scraped = 0
-
-            while comments_scraped < 30:
-                scroll_popup(post_popup)
-                new_comments = post_popup.find_elements(By.XPATH, ".//div[@class='x1lliihq xjkvuk6 x1iorvi4']")
-
-                for new_comment in new_comments:
-                    comment_text = new_comment.text
-                    query = """
-                            SELECT COUNT(*) 
-                            FROM Comments 
-                            WHERE content LIKE %s
-                            """
-                    cursor.execute(query, (comment_text,))
-                    count = cursor.fetchone()[0]
-
-                    if count > 0:
-                        continue
-
-                    comment_uuid = str(uuid.uuid4())
-
-                    sql_insert_comment = """
-                                        INSERT IGNORE
-                                        INTO Comments (uuid, post_id, content)
-                                        VALUES(%s, %s, %s)
-                                        """
-                             
-                    cursor.execute(sql_insert_comment, (comment_uuid, post_uuid, comment_text))
-                    connection.commit()
-                    comments_scraped += 1
-                    time.sleep(1)
-                    
-            print(f"Retrieved {comments_scraped} comments")
-            close_button = driver.find_element(By.XPATH, '//div[@class="x1i10hfl xjqpnuy xa49m3k xqeqjp1 x2hbi6w x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x1ypdohk xdl72j9 x2lah0s xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r x2lwn1j xeuugli x16tdsg8 x1hl2dhg xggy1nq x1ja2u2z x1t137rt x1q0g3np x87ps6o x1lku1pv x1a2a7pz x6s0dn4 xzolkzo x12go9s9 x1rnf11y xprq8jg x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x78zum5 xl56j7k xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 xc9qbxq x14qfxbe x1qhmfi1"]')
-            close_button.click()
         scroll_down()
         
     print("##### Scraping finished #####")
